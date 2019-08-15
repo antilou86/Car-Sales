6c09f92b4ac56c000865acb669f24b62ea7aeca5
antilou86 <antilou86@gmail.com>
2019-08-15 11:44:31 -0400

    added logger middleware for warmup excersize.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from 'redux';
 import { carReducer } from './reducers';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+
+import logger from 'redux-logger'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-export const store = createStore(carReducer);
+export const store = createStore(carReducer, applyMiddleware(logger));
 
 const rootElement = document.getElementById('root');
 ReactDOM.render( <Provider store={store}> <App /></Provider>, rootElement);
